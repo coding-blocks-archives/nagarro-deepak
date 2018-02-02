19a475e63d2008ca5328f84c95921c46f95964a5
Deepak Aggarwal <daggarwal77@gmail.com>
2018-02-02 11:41:50 +0530

    LevelWise

--- 05_trees/Tree.cs
+++ 05_trees/Tree.cs
@@ -17,9 +17,9 @@ namespace nagarro_deepak
             }
 
             TreeNode root = new TreeNode(x);
-            Console.WriteLine("Enter left child of "  + root.data + " ");
+            // Console.WriteLine("Enter left child of "  + root.data + " ");
             root.left = createTree();
-            Console.WriteLine("Enter right child of "  + root.data + " ");
+            // Console.WriteLine("Enter right child of "  + root.data + " ");
             root.right = createTree();
             return root;
         }
@@ -27,16 +27,39 @@ namespace nagarro_deepak
         public static void printLevelOrder(TreeNode root)
         {
             Queue<TreeNode> q = new Queue<TreeNode>();
+            const TreeNode MARKER = null;
+
             if (root != null) q.Enqueue(root);
+            q.Enqueue(MARKER);
 
             // processing
             while (q.Count != 0)
             {
                 TreeNode cur = q.Dequeue();
+                if (cur == MARKER){
+                    if (q.Count != 0){
+                        Console.WriteLine();
+                        q.Enqueue(MARKER);
+                        continue;
+                    }
+                    else {
+                        break;
+                    }
+                }
                 Console.Write(cur.data + " ");
                 if (cur.left != null) q.Enqueue(cur.left);
                 if (cur.right != null) q.Enqueue(cur.right);
             }
         }
+
+        // height(){}
+        // arrToBinary(){}
+        // heightBalanced(){}
+        // zigZag(){}
+        // TopView(){}
+        // LowestCommonAncestor(){}
+        // nextRightPointers(){}
+        // binaryTreeFromInorder(){}
+
     }
 }
