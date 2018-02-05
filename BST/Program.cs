44e795b4412dbebaa6bcebfec314bd9eb36f75be
Abhishek Raj <rai.abhishekraj@gmail.com>
2018-02-05 14:19:14 +0530

    BST Implementation (Basic) (#1)
    BST Implementation

---
+++ BST/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode root = null;
+            BinarySearchTree bst = new BinarySearchTree();
+            Console.WriteLine("Enter the number of nodes you want to enter:- ");
+            int nodeNums = int.Parse(Console.ReadLine());
+            for (int nodeNum = 0; nodeNum < nodeNums; nodeNum++)
+            {
+                Console.WriteLine($"Enter data No. {nodeNum + 1}");
+                int data = int.Parse(Console.ReadLine());
+                bst.InsertNode(ref root, data);
+            }
+            // call the print method
+            Console.WriteLine("Enter");
+            bst.PreOrderPrintTree(root);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BST/TreeNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace BST
+{
+    class TreeNode
+    {
+        public int data;
+        public TreeNode left, right;
+        public TreeNode() { }
+        public TreeNode(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+    }
+}
